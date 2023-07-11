e372e2ee6506b2c6d1d9e8a7176251e8a9acb682
Daniel <danmcdonald95@outlook.com>
2023-07-10 23:46:01 -0700

    Completed Lab

---
+++ index.js
@@ -0,0 +1,52 @@
+number = 15;
+
+
+function add() {
+  return (number += 8);
+}
+
+function divide() {
+  return (number /= 4);
+}
+
+function subtract() {
+    return (number -= 2);
+  }
+
+  function multiply() {
+    return (number *= 9);
+  }
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+   return parseFloat(n);
+    
+}
+
+function increment(n) {
+    n++
+    return n;
+}
+
+function decrement(n) {
+    n--
+    return n;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
